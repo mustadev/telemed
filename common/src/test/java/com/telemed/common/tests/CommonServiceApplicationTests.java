774a4585d27bc64524c11956391ad35c7e9b8d85
mustadev <ouarrain.mustapha@gmail.com>
2020-06-01 02:04:24 +0200

    gateway created

---
+++ common/src/test/java/com/telemed/common/tests/CommonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telemed.common.tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class CommonServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
---
+++ gateway/src/test/java/com/telemed/gateway/tests/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.telemed.gateway.tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GatewayApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
